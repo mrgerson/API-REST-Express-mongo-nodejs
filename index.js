1ab54dd17654acb948a8d41cfc580a0b4abb494a
mrgerson <gcalvo@tecbaco.com>
2023-08-02 16:18:57 -0500

    actualizando validacion en los cors

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
 app.use(
     cors({
         origin: function (origin, callback) {
-            if (whiteList.includes(origin)) {
+            console.log(origin)
+            if (!origin ||  whiteList.includes(origin)) {
                 return callback(null, origin);
             }
             return callback(
